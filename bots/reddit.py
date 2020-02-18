32817a7b8ede15d62104e050abe08cee18ecf846
DuskoDusko <58344946+DuskoDusko@users.noreply.github.com>
2020-02-18 18:44:35 +0100

    yeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeet

--- bots/reddit.py
+++ bots/reddit.py
@@ -1,21 +1,55 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from time import sleep
-
-mail = "matus.duchyna@gmail.com"
+from random import random
 
 class bot:
+
     def __init__(self):
         chrome_options = Options()
         chrome_options.add_argument(
             '--user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1')
+        sleep(1)
         self.driver = webdriver.Chrome("C:\chromedriver_win32\chromedriver.exe", options=chrome_options)
+        sleep(1)
         self.driver.get('https://www.reddit.com/')
+        sleep(1)
         self.driver.maximize_window()
+        sleep(1)
         self.driver.find_element_by_xpath(
             '//*[@id="container"]/div/div[1]/div[1]/div[1]/div/div[2]/div[2]/div[2]/button').click()
-        self.driver.find_element_by_xpath('//*[@id="container"]/div/div[1]/div[1]/div/nav/ul/li[3]/button').click()
-        self.driver.find_element_by_xpath('//*[@id="container"]/div/nav/ul/a').click()
-        self.driver.find_element_by_xpath('//*[@id="email"]').send_keys(mail)
+        sleep(1)
+        self.driver.find_element_by_xpath('//*[@id="container"]/div/div[1]/div[1]/div/nav/ul/li[5]/button').click()
+        sleep(1)
+        self.driver.find_element_by_xpath('//*[@id="container"]/div/nav/ul/li[1]/a').click()
+        self.upvotes = []
+        print('please log in')
+        print('\"bot.toMemes()\" to continue')
+
+    def toMemes(self):
+        self.driver.find_element_by_xpath('//*[@id="container"]/div/div[1]/div[1]/div/nav/ul/li[4]/button').click()
+        sleep(1)
+        self.driver.find_element_by_xpath('//*[@id="container"]/div/nav/ul/div/label/div/input').send_keys('memes')
+        sleep(1)
+        self.driver.find_element_by_xpath('//*[@id="container"]/div/nav/ul/div/div/ul/li[1]/a').click()
+        sleep(1)
         self.driver.find_element_by_xpath(
-            '//*[@id="container"]/div/div[1]/div[1]/div/div/div[2]/div/div[3]/form/div[5]/button').click()
+            '//*[@id="container"]/div/div[1]/div[1]/div/div/div/div/nav/div[1]/div[1]/div/div[1]/span[2]').click()
+        sleep(1)
+        self.driver.find_element_by_xpath(
+            '//*[@id="container"]/div/div[1]/div[1]/div/div/div/div/nav/div[1]/div[1]/div/div[2]/div/div[3]/div[4]/div[2]').click()
+        sleep(1)
+        self.start()
+
+    def start(self):
+        self.upvotes = self.driver.find_elements_by_class_name('icon-upvote')
+        sleep(2)
+        for upvote in self.upvotes:
+            sleep(random())
+            upvote.click()
+        print("page done")
+        self.driver.refresh()
+        sleep(2)
+        self.start()
+
+print("\"bot = bot()\" to start")
