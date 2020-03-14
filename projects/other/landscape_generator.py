d1d30367595b9067f565942298f5a889a0ffe694
DuskoDusko <58344946+DuskoDusko@users.noreply.github.com>
2020-03-14 11:19:19 +0100

    .

--- projects/other/landscape_generator.py
+++ projects/other/landscape_generator.py
@@ -1,62 +1,57 @@
+from opensimplex import OpenSimplex
 import tkinter
-from random import randrange
+from random import randint
 
-window = tkinter.Canvas(height = 500, width = 500)
-window.pack()
+########################################################################################################################
 
-krok = 10
-sirka = 2
+colors = [
+    [-0.3, 'blue'],
+    [-0.2, 'yellow'],
+    [0.7, 'green'],
+    [0.9, 'gray'],
+    [1, 'white']
+]
 
-def reset(self=None):
-    farba = 'black'
-    color = []
-    window.delete('ALL')
-    color = [[0 for x in range(500//sirka)] for y in range(500//sirka)]
-    for x in range(500//sirka):
-        for y in range(500//sirka):
-            if x == 0 and y == 0:
-                color[0][0] = randrange(0, 250//krok)*krok
-            else:
-                step = randrange(0, 2)
-                if step == 0:
-                    if y == 0:
-                        color[x][y] = color[x-1][y]-krok
-                        if color[x][y] < 0:
-                            color[x][y] = 0
-                    elif x == 0:
-                        color[x][y] = color[x][y-1]-krok
-                        if color[x][y] < 0:
-                            color[x][y] = 0
-                    else:
-                        color[x][y] = (color[x-1][y]+color[x][y-1])//2//10*10-krok
-                        if color[x][y] < 0:
-                            color[x][y] = 0
-                if step == 1:
-                    if y == 0:
-                        color[x][y] = int(color[x-1][y]+krok*1.4)
-                        if color[x][y] > 250:
-                            color[x][y] = 250
-                    elif x == 0:
-                        color[x][y] = int(color[x][y-1]+krok*1.4)
-                        if color[x][y] > 250:
-                            color[x][y] = 250
-                    else:
-                        color[x][y] = int((color[x-1][y]+color[x][y-1])//2//10*10+krok*1.4)
-                        if color[x][y] > 250:
-                            color[x][y] = 250
-            if 0 <= color[x][y] < 50:
-                farba = 'blue'
-            elif 50 < color[x][y] < 100:
-                farba = 'LightGoldenrod2'
-            elif 100 < color[x][y] < 150:
-                farba = 'green'
-            elif 150 < color[x][y] < 200:
-                farba = 'saddlebrown'
-            elif 200 < color[x][y] <= 250:
-                farba = 'white'
-            window.create_rectangle(x*sirka, y*sirka, x*sirka+sirka, y*sirka+sirka, fill = farba, width = 0)
+freq = 10
 
-reset()
-window.bind_all('r', reset)      
+windowWidth, windowHeight = 1400, 800
+cellsX, cellsY = 1400, 800
+cellsWidth, cellsHeight = windowWidth/cellsX, windowHeight/cellsY
 
-window.mainloop()
+########################################################################################################################
+
+c = tkinter.Canvas(width=windowWidth, height=windowHeight)
+c.pack()
+
+seed = randint(0, 10000)
+gen = OpenSimplex(seed=seed)
+print(seed)
+
+cells = [[c.create_rectangle(x*cellsWidth, y*cellsHeight, x*cellsWidth+cellsWidth, y*cellsHeight+cellsHeight, width=0)
+         for x in range(cellsX)] for y in range(cellsY)]
+
+def noise(nx, ny):
+    e = gen.noise2d(nx*freq, ny*freq)
+    return e
+
+def getColor(value):
+    for x in colors:
+        if value < x[0]:
+            return x[1]
+
+min = 2
+max = 0
+
+for y in range(cellsY):
+    for x in range(cellsX):
+        nx = x/cellsX - 0.5
+        ny = y/cellsY - 0.5
+        v = noise(nx, ny)
+        c.itemconfig(cells[y][x], fill=getColor(v))
+        if v < min:
+            min = v
+        if v > max:
+            max = v
+print(min, max)
+
+c.mainloop()
