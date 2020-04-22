251427818983d2467cea22ce3b6874baccf5faa0
DuskoDusko <58344946+DuskoDusko@users.noreply.github.com>
2020-04-22 13:01:36 +0200

    test

--- test.py
+++ test.py
@@ -1,21 +1,36 @@
 # 1
-file = open('abeceda.txt', 'w')
-for i in range(65, 91):
-    file.write(chr(i) + '=' + str(i) + '\n')
-file.close()
+import tkinter
 
-# 2
-file = open('abeceda.txt', 'w')
-for i in range(65, 91):
-    file.write(chr(i) + '=' + str(i) + '\n')
-for i in range(97, 123):
-    file.write(chr(i) + '=' + str(i) + '\n')
-file.close()
+canvas = tkinter.Canvas(width = 500, height = 500)
+canvas.pack()
 
-# 3
-file = open('cisla.txt', 'r')
+file = open('spokojnost.txt', 'r')
+ano = 0
+nie = 0
 riadok = file.readline()
 while riadok != '':
-    print(riadok.strip())
+    if riadok.strip() == 'ano':
+        ano = ano + 1
+    if riadok.strip() == 'nie':
+        nie = nie + 1
     riadok = file.readline()
-file.close()
+dokopy = ano + nie
+percentoAno = ano / dokopy * 100
+percentoNie = nie / dokopy * 100
+print('Pocet hlasujucic: ' + str(dokopy))
+print('Percento ano: ' + str(percentoAno) + '%')
+print('Percento nie: ' + str(percentoNie) + '%')
+
+canvas.create_text(150, 450, text='áno')
+canvas.create_text(350, 450, text='nie')
+canvas.create_rectangle(100, 400-ano*2, 200, 400, fill='red')
+canvas.create_rectangle(300, 400-nie*2, 400, 400, fill='blue')
+
+canvas.mainloop()
+
+# # 2
+# file = open('kody.txt', 'w')
+# for x in range(10):
+#     for y in range(10):
+#         for z in range(10):
+#             file.write(str(x) + str(y) + str(z) + '\n')
