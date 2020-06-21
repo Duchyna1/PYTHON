6e721cceda6c86ac448ed2cc07b4c45adb0aa4cb
Dusko <51196365+Duchyna1@users.noreply.github.com>
2020-06-21 12:52:14 +0200

    wait

--- projects/raspberrypi/client.py
+++ projects/raspberrypi/client.py
@@ -1,10 +1,12 @@
 import socket
+import time
 
 HOST = '192.168.0.38'
 PORT = 65432
 
 def send(s, messange):
     s.sendall(str.encode(messange))
+    time.sleep(0.5)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
