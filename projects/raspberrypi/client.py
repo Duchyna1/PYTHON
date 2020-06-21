2a8ebbd18c928ff104ba5f9982a7d98e17f16bc6
Dusko <51196365+Duchyna1@users.noreply.github.com>
2020-06-21 12:58:56 +0200

    ja uz neviem

--- projects/raspberrypi/client.py
+++ projects/raspberrypi/client.py
@@ -4,12 +4,14 @@ import time
 HOST = '192.168.0.38'
 PORT = 65432
 
+
 def send(s, messange):
+    s.connect((HOST, PORT))
     s.sendall(str.encode(messange))
     time.sleep(0.5)
 
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
     send(s, 'Hello')
     send(s, 'Wait')
     send(s, 'Hello2')
--- projects/raspberrypi/server.py
+++ projects/raspberrypi/server.py
@@ -6,7 +6,6 @@ PORT = 65432
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
-    print("listen")
     conn, addr = s.accept()
     with conn:
         print('Connected by ', addr)
@@ -14,3 +13,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             data = conn.recv(1024)
             if data:
                 print(data.decode("utf-8"))
+            else:
+                s.listen()
+                conn, addr = s.accept()
