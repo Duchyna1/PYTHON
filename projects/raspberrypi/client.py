ed14c26c27c9bd350beee1cd8bd9335bc768c8c7
Dusko <51196365+Duchyna1@users.noreply.github.com>
2020-06-21 12:47:57 +0200

    client

--- projects/raspberrypi/client.py
+++ projects/raspberrypi/client.py
@@ -3,11 +3,11 @@ import socket
 HOST = '192.168.0.38'
 PORT = 65432
 
-def send(messange):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-        s.sendall(str.encode(messange))
+def send(s, messange):
+    s.sendall(str.encode(messange))
 
-send('Hello')
-send('Wait')
-send('Hello2')
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    send(s, 'Hello')
+    send(s, 'Wait')
+    send(s, 'Hello2')
