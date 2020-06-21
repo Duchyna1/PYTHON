a00cdd873ea4b0bb78e8d5e7ba82b9d9366210aa
Dusko <51196365+Duchyna1@users.noreply.github.com>
2020-06-21 12:32:43 +0200

    back

--- projects/raspberrypi/push.py
+++ projects/raspberrypi/push.py
@@ -2,10 +2,7 @@ import os
 
 message = input("Commit message: ")
 
-print(os.getcwd())
 os.system('cd /')
-print(os.getcwd())
 os.system('cd Users/matus/OneDrive/Počítač/GIT/PYTHON')
-print(os.getcwd())
 os.system('git commit -am "{}"'.format(message))
 os.system('git push')
